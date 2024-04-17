ddf511b96d3e70852bbddd704b4eec677ce179cd
Ainesh Sinha <studentainesh@gmail.com>
2024-04-17 14:20:07 +0530

    First commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
+const Hero = () => {
+    useGSAP(() => {
+        gsap.to('#hero', {
+            opacity: 1,
+            delay: 1.5,
+        })
+    }, [])
+
+  return (
+    <section className='w-full nav-height bg-black relative'>
+        <div className='h-5/6 w-full flex-center flex-col'>
+            <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
